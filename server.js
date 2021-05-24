8591264e114a45e26f02ab4793ef405383322ef7
Zuzanna Szeloch <zuz.szeloch@gmail.com>
2021-05-25 00:30:00 +0200

    Add exercise 21.4 - develop express

--- server.js
+++ server.js
@@ -1,41 +1,60 @@
-const express = require('express');
-const path = require('path');
-const hbs = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+app.engine(".hbs", hbs());
+app.set("view engine", ".hbs");
 
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.get('/', (req, res) => {
-  res.render('index', { layout: false });
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
 });
 
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { name: req.params.name });
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: req.params.name });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/info", (req, res) => {
+  res.render("info");
+});
+app.get("/history", (req, res) => {
+  res.render("history", { layout: "dark" });
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
-});
+app.post("/contact/send-message", upload.single("yourFile"), (req, res) => {
+  try {
+    const { author, sender, title, message } = req.body;
+    const { filename, originalname } = req.file;
 
-app.get('/info', (req, res) => {
-  res.render('info');
-});
-
-app.get('/history', (req, res) => {
-  res.render('history', { layout: 'dark' });
+    if (author && sender && title && message && filename) {
+      res.render("contact", { isSent: true, yourFileName: originalname });
+    }
+  } catch (error) {
+    res.render("contact", { isError: true });
+  }
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
-})
-
+  res.status(404).send("404 not found...");
+});
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+  console.log("serwer is running on port: 8000");
 });
